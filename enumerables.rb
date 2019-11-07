5e67caaea3f1e1c361bf3defc8bc39be31b022f8
Luis Puente <puente.luism@gmail.com>
2019-11-07 15:24:36 -0600

    Add each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -13,4 +13,39 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return self.to_enum unless block_given?
+
+    if self.is_a? Array
+      i = 0
+      while i < self.length do
+        yield(self[i], i)
+          i = i+1
+      end
+    elsif self.is_a? Hash
+      j = 0
+      while j < self.length do
+        yield([self.keys[j], self.values[j]], j)
+          j += 1
+      end
+    end
+    self
+  end
+
+
 end
+
+=begin
+t1 = [1,2,3,4,5]
+t2 = {a:1, b:2, c:3, d:4}
+t3 = ["do", "don't", "memee"]
+t4 = {mi:"mama", me:"mima", mina:"moa", al:"together"}
+
+#t1.my_each_with_index{|item,index| puts "#{item} and #{index} here"}
+t2.my_each_with_index{|item,index| puts "#{item} and #{index} here"}
+t2.my_each_with_index{|(key,value), i| puts "key is #{key} value is #{value} index is #{i}"}
+t4.my_each_with_index{|(key,value), i| puts "#{key} and #{value} here and #{i} here"}
+#t3.my_each_with_index{|(key,value), i| puts "#{key} and #{value} here and #{i} here"}
+
+=end
