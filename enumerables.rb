36190346ab7221d361367eb590648087aeb6202b
Luis Puente <puente.luism@gmail.com>
2019-11-07 15:59:46 -0600

    Add my_select method

--- enumerables.rb
+++ enumerables.rb
@@ -33,15 +33,38 @@ module Enumerable
     self
   end
 
+  def my_select
+    return self.to_enum unless block_given?
+
+    if self.is_a? Array
+      myarr = []
+      self.my_each do |item|
+        myarr.push(item) if yield(item)
+      end
+    elsif self.is_a? Hash 
+      myarr = {}
+      self.my_each do |k,v|
+        myarr[k]=v if yield(k,v)
+      end
+    end
+  myarr
+  end
 
 end
 
-=begin
+#----samples
 t1 = [1,2,3,4,5]
 t2 = {a:1, b:2, c:3, d:4}
 t3 = ["do", "don't", "memee"]
 t4 = {mi:"mama", me:"mima", mina:"moa", al:"together"}
 
+#----tests
+puts t1.my_select{|x| x <= 3}
+puts t2.my_select{|x,y| y >= 3}  #{:c=>3, :d=>4}
+
+
+
+=begin
 #t1.my_each_with_index{|item,index| puts "#{item} and #{index} here"}
 t2.my_each_with_index{|item,index| puts "#{item} and #{index} here"}
 t2.my_each_with_index{|(key,value), i| puts "key is #{key} value is #{value} index is #{i}"}
