da0d16aec79072887530a3a1ab759ef431c90755
Luis Puente <puente.luism@gmail.com>
2019-11-08 21:34:41 -0600

    Updated linter

--- enumerables.rb
+++ enumerables.rb
@@ -1,31 +1,29 @@
 # frozen_string_literal: true
 
-module Enumerable # rubocop:disable Metrics/ModuleLength
+module Enumerable # rubocop:disable Metrics/ModuleLength  ///////////
   def my_each
-    return self.to_enum unless block_given?
-      
-    if self.is_a? Array
+    return to_enum unless block_given?
+    if is_a? Array
       for i in self do
         yield(i)
       end       
-    elsif self.is_a? Hash
+    elsif is_a? Hash
       for key,value in self do
-        yield(key,value)
+        yield(key, value)
       end  
     end
     self
   end
 
   def my_each_with_index
-    return self.to_enum unless block_given?
-
-    if self.is_a? Array
+    return to_enum unless block_given?
+    if is_a? Array
       i = 0
       while i < self.length do
         yield(self[i], i)
-          i = i+1
+          i = i + 1
       end
-    elsif self.is_a? Hash
+    elsif is_a? Hash
       j = 0
       while j < self.length do
         yield([self.keys[j], self.values[j]], j)
@@ -36,25 +34,23 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
   end
 
   def my_select
-    return self.to_enum unless block_given?
-
-    if self.is_a? Array
+    return to_enum unless block_given?
+    if is_a? Array
       myarr = []
       self.my_each do |item|
         myarr.push(item) if yield(item)
       end
-    elsif self.is_a? Hash 
+    elsif is_a? Hash 
       myarr = {}
       self.my_each do |k,v|
-        myarr[k]=v if yield(k,v)
+        myarr[k]=v if yield(k, v)
       end
     end
   myarr
   end
 
   def my_all?(pattern=nil)
-    return true unless block_given? || !pattern.nil?
-    
+    return true unless block_given? || !pattern.nil?   
     if pattern
       self.my_each do |item|
         return false unless item.is_a? pattern
@@ -64,8 +60,8 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
         return false unless yield(item)
       end 
     elsif self.is_a? Hash
-      self.my_each do |k,v|
-        return false unless yield(k,v)
+      self.my_each do |k, v|
+        return false unless yield(k, v)
       end
     end
     true
@@ -73,19 +69,17 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
 
   def my_any?(pattern=nil)
     return false unless block_given? || !pattern.nil?
-    
     if pattern
       self.my_each do |item|
         return true unless !item.is_a? pattern
       end
-
     elsif self.is_a? Array
       self.my_each do |item|
         return true if yield(item)
       end 
     elsif self.is_a? Hash
-      self.my_each do |k,v|
-        return true if yield(k,v)
+      self.my_each do |k, v|
+        return true if yield(k, v)
       end
     end
     false
@@ -93,7 +87,6 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
 
   def my_none?(pattern=nil)
   return true unless block_given? || !pattern.nil?
-    
     if pattern
       self.my_each do |item|
         return true unless item.is_a? pattern
@@ -103,8 +96,8 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
         return true unless yield(item)
       end 
     elsif self.is_a? Hash
-      self.my_each do |k,v|
-        return false if yield(k,v)
+      self.my_each do |k, v|
+        return false if yield(k, v)
       end
     end
     false
@@ -112,7 +105,6 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
 
   def my_count(x=nil)
     return self.length unless block_given? || !x.nil?
-
     counter = 0
     if !x.nil? 
       self.my_each do |item|
@@ -127,7 +119,7 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
         end
       end
     elsif self.is_a? Hash
-      self.my_each do |k,v|
+      self.my_each do |k, v|
         if yield(k,v)
           counter +=1
         end
@@ -138,15 +130,14 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
 
   def my_map
     return self.to_enum unless block_given?
-    
     new_array = []
     if self.is_a? Array
       self.my_each do |k|
         new_array.push(yield(k))
       end
     elsif self.is_a? Hash
-      self.my_each do |k,v|
-        new_array.push(yield(k,v))
+      self.my_each do |k, v|
+        new_array.push(yield(k, v))
       end
     end
         new_array
@@ -159,7 +150,7 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
       result = args[0].nil? ? my_arr[0] : args[0]
       my_arr.shift if args[0].nil?
       my_arr.each do |number|
-        result = yield(result,number)
+        result = yield(result, number)
       end
     elsif !block_given? 
       my_arr = self.to_a
@@ -167,9 +158,8 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
         symbol = args[0]
         result = my_arr[0]
         my_arr[1..-1].my_each do |i|
-        result = result.send(symbol,i)
-        end
-      
+        result = result.send(symbol, i)
+        end      
       elsif !args[1].nil?
         symbol = args[1]
         result = args[0]
@@ -186,6 +176,5 @@ def multiply_els(a)
   a.my_inject(:*)
 end
 p multiply_els([2,4,5])
-
 double = Proc.new { |num| num*2 }
 p [1, 2, 3].my_map(&double).my_map{ |num| num*2 }
