c8986dcf5c7067606fbffa5e03960e2b1d3dbaab
Luis Puente <puente.luism@gmail.com>
2019-11-10 18:16:11 -0600

    Fix stickler all errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,33 +1,39 @@
+# frozen_string_literal: true
+
 module Enumerable
   def my_each
     return to_enum unless block_given?
-    arr_to_work = self
-    if arr_to_work.is_a? Array
-      for i in arr_to_work do
-        yield(i)
-      end       
-    elsif arr_to_work.is_a? Hash
-      for key,value in arr_to_work do
-        yield(key,value)
-      end  
+
+    if is_a? Array
+      i = 0
+      while i < size
+        yield(self[i])
+        i += 1
+      end
+    elsif is_a? Hash
+      while i < size
+        yield(values[i], keys[i])
+        i += 1
+      end
     end
     self
   end
 
   def my_each_with_index
     return to_enum unless block_given?
+
     arr_to_work = self
     if arr_to_work.is_a? Array
       i = 0
-      while i < arr_to_work.length do
+      while i < arr_to_work.length
         yield(arr_to_work[i], i)
-          i = i+1
+        i += 1
       end
     elsif arr_to_work.is_a? Hash
       j = 0
-      while j < arr_to_work.length do
+      while j < arr_to_work.length
         yield([arr_to_work.keys[j], arr_to_work.values[j]], j)
-          j += 1
+        j += 1
       end
     end
     self
@@ -35,103 +41,99 @@ module Enumerable
 
   def my_select
     return to_enum unless block_given?
+
     arr_to_work = self
-    if arr_to_work.is_a? Array
+    if is_a? Array
       myarr = []
       arr_to_work.my_each do |item|
         myarr.push(item) if yield(item)
       end
-    elsif arr_to_work.is_a? Hash 
+    elsif is_a? Hash
       myarr = {}
-      arr_to_work.my_each do |k,v|
-        myarr[k]=v if yield(k,v)
+      arr_to_work.my_each do |k, v|
+        myarr[k] = v if yield(k, v)
       end
     end
-  myarr
+    myarr
   end
 
-  def my_all?(pattern = nil)
+  def my_all?(pattern = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     return true unless block_given? || !pattern.nil?
+
     arr_to_work = self
     if pattern
       arr_to_work.my_each do |item|
         return false unless item.is_a? pattern
       end
-    elsif arr_to_work.is_a? Array
+    elsif is_a? Array
       arr_to_work.my_each do |item|
         return false unless yield(item)
-      end 
-    elsif arr_to_work.is_a? Hash
-      arr_to_work.my_each do |k,v|
-        return false unless yield(k,v)
+      end
+    elsif is_a? Hash
+      arr_to_work.my_each do |k, v|
+        return false unless yield(k, v)
       end
     end
     true
   end
 
-  def my_any?(pattern=nil)
+  def my_any?(pattern = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     return false unless block_given? || !pattern.nil?
+
     arr_to_work = self
-    
     if pattern
       arr_to_work.my_each do |item|
-        return arr_to_work unless !item.is_a? pattern
+        return true if item.is_a? pattern
       end
 
-    elsif arr_to_work.is_a? Array
+    elsif is_a? Array
       arr_to_work.my_each do |item|
         return true if yield(item)
-      end 
-    elsif arr_to_work.is_a? Hash
-      arr_to_work.my_each do |k,v|
-        return true if yield(k,v)
+      end
+    elsif is_a? Hash
+      arr_to_work.my_each do |k, v|
+        return true if yield(k, v)
       end
     end
     false
   end
 
-  def my_none?(pattern = nil)
-  return true unless block_given? || !pattern.nil?
-  arr_to_work = self
+  def my_none?(pattern = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    return true unless block_given? || !pattern.nil?
+
+    arr_to_work = self
     if pattern
       arr_to_work.my_each do |item|
         return true unless item.is_a? pattern
       end
-    elsif arr_to_work.is_a? Array
+    elsif is_a? Array
       arr_to_work.my_each do |item|
         return true unless yield(item)
-      end 
-    elsif arr_to_work.is_a? Hash
-      arr_to_work.my_each do |k,v|
-        return false if yield(k,v)
+      end
+    elsif is_a? Hash
+      arr_to_work.my_each do |k, v|
+        return false if yield(k, v)
       end
     end
     false
   end
 
-  def my_count(x = nil)
-    return length unless block_given? || !x.nil?
+  def my_count(xxx = nil) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    return length unless block_given? || !xxx.nil?
 
     arr_to_work = self
-
     counter = 0
-    if !x.nil? 
+    if !xxx.nil?
       arr_to_work.my_each do |item|
-        if item == x
-          counter +=1
-        end
+        counter += 1 if item == xxx
       end
-    elsif arr_to_work.is_a? Array
+    elsif is_a? Array
       arr_to_work.my_each do |item|
-        if yield(item)
-          counter +=1
-        end
+        counter += 1 if yield(item)
       end
-    elsif arr_to_work.is_a? Hash
-      arr_to_work.my_each do |k,v|
-        if yield(k,v)
-          counter +=1
-        end
+    elsif is_a? Hash
+      arr_to_work.my_each do |k, v|
+        counter += 1 if yield(k, v)
       end
     end
     counter
@@ -139,42 +141,38 @@ module Enumerable
 
   def my_map
     return to_enum unless block_given?
-    
-    arr_to_work = self
 
+    arr_to_work = self
     new_array = []
-    if arr_to_work.is_a? Array
+    if is_a? Array
       arr_to_work.my_each do |k|
         new_array.push(yield(k))
       end
-    elsif arr_to_work.is_a? Hash
-      arr_to_work.my_each do |k,v|
-        new_array.push(yield(k,v))
+    elsif is_a? Hash
+      arr_to_work.my_each do |k, v|
+        new_array.push(yield(k, v))
       end
     end
-        new_array
+    new_array
   end
 
-  def my_inject(*args)
-
-    arr_to_work = self
-    my_arr = arr_to_work.to_a
+  def my_inject(*args) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    my_arr = to_a
     if block_given?
-      my_arr = arr_to_work.to_a
+      my_arr = to_a
       result = args[0].nil? ? my_arr[0] : args[0]
       my_arr.shift if args[0].nil?
       my_arr.each do |number|
-        result = yield(result,number)
+        result = yield(result, number)
       end
-    elsif !block_given? 
-      my_arr = arr_to_work.to_a
+    elsif !block_given?
+      my_arr = to_a
       if args[1].nil?
         symbol = args[0]
         result = my_arr[0]
         my_arr[1..-1].my_each do |i|
-        result = result.send(symbol,i)
+          result = result.send(symbol, i)
         end
-      
       elsif !args[1].nil?
         symbol = args[1]
         result = args[0]
@@ -185,14 +183,14 @@ module Enumerable
     end
     result
   end
-
 end
 
-def multiply_els(a)
-  a.my_inject(:*)
+def multiply_els(arg)
+  arg.my_inject(:*)
 end
-p multiply_els([2,4,5])
+p multiply_els([2, 4, 5])
 
-
-double = Proc.new { |num| num*2 }
-p [1, 2, 3].my_map(&double).my_each{ |num| num*2 }
+# double = Proc.new { |num| num * 2 }
+# p [1, 2, 3].my_map(&double).my_map { |num| num * 2 }
+abc = [a: 2, b: 3, c: 4]
+abc.my_each { |a| puts "a #{a} and b #{a}" }
